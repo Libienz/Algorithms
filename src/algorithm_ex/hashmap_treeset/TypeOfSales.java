2a621756e28af7a1dc246b43a3b779b86b51345a
Lkh-Dev <Leekh10195@gmail.com>
2022-07-14 07:57:26 +0900

    04-03 done

--- src/algorithm_ex/hashmap_treeset/TypeOfSales.java
+++ src/algorithm_ex/hashmap_treeset/TypeOfSales.java
@@ -8,12 +8,23 @@ public class TypeOfSales {
 
     public static void solution(int days, int win, int[] arr) {
         Map<Integer,Integer> map = new HashMap<>();
-        for (int i= 0; i<=days-win; i++) {
-            for (int j = i; j<i+win; j++) {
-                map.put(arr[j],map.getOrDefault(arr[j],0) + 1);
+        for (int i = 0; i<win; i++) {
+            map.put(arr[i],map.getOrDefault(arr[i],0) + 1);
+        }
+        //처음 4개 done
+        System.out.print(map.size() + " ");
+
+        for (int i= 1; i<=days-win; i++) {
+            int lt = i-1; //빼야하는 애
+            int rt = lt+win; //더해야 하는 애
+            if(map.get(arr[lt]) -1 == 0) {
+                map.remove(arr[lt]);
             }
+            else{
+                map.put(arr[lt],map.get(arr[lt])-1);
+            }
+            map.put(arr[rt],map.getOrDefault(arr[rt],0) + 1);
             System.out.print(map.size() + " ");
-            map.clear();
         }
     }
     public static void main(String[] args) {
