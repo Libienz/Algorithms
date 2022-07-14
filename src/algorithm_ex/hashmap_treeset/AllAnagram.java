153f207d29813dddf0ca8c8c028e6da5cbe9e319
Lkh-Dev <Leekh10195@gmail.com>
2022-07-15 08:48:51 +0900

    04-04 done

--- src/algorithm_ex/hashmap_treeset/AllAnagram.java
+++ src/algorithm_ex/hashmap_treeset/AllAnagram.java
@@ -14,11 +14,12 @@ import java.util.Scanner;
 public class AllAnagram {
 
     public static boolean isSameMap(Map<Character,Integer> map, Map<Character,Integer> obj_map) {
-        for (char key : obj_map.keySet()) {
+        /*for (char key : obj_map.keySet()) {
             if (!map.containsKey(key)) return false;
             if (map.get(key) != obj_map.get(key)) return false;
         }
-        return true;
+        return true;*/
+        return map.equals(obj_map);
     }
 
     public static int findAllAnagram(String str1, String str2) {
@@ -44,6 +45,9 @@ public class AllAnagram {
             int lt = i - 1;
             int rt = lt + str2.length();
             map.put(str1_arr[lt], map.get(str1_arr[lt])-1); // 빼야할 것 빼고
+            if (map.get(str1_arr[lt]) == 0) {
+                map.remove(str1_arr[lt]);
+            }
             map.put(str1_arr[rt], map.getOrDefault(str1_arr[rt],0)+1); //더해야할 것 더하면 새로운 슬라이드에 대한 맵 완성
 
             if (isSameMap(map,obj_map)) count++;
