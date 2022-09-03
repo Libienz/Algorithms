de0e1d4feb0283fd690353a9afca6d0ed3ee3f18
Lkh-dev <Leekh10195@gmail.com>
2022-09-03 17:13:26 +0900

    10-03 in progress..

---
+++ src/algorithm_ex/greedy/LIS.java
@@ -0,0 +1,48 @@
+package algorithm_ex.greedy;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class LIS {
+
+    public static ArrayList<Integer> seq = new ArrayList<>();
+    public static int answer = Integer.MIN_VALUE;
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int length = sc.nextInt();
+
+        //수열 작성
+        for (int i = 0; i < length; i++) {
+            seq.add(sc.nextInt());
+        }
+
+        dfs(0, Integer.MIN_VALUE, 0);
+        System.out.println(answer);
+
+    }
+
+    public static void dfs(int index, int prev, int length) {
+
+        if (index == seq.size()) {
+            if (length > answer) {
+                answer = length;
+            }
+        }
+        else {
+
+            if (seq.get(index) < prev) {
+                dfs(index + 1, prev, length);
+            }
+            else {
+                dfs(index + 1, seq.get(index), length + 1);
+                dfs(index + 1, prev, length);
+            }
+
+        }
+    }
+    private static int getLis(ArrayList<Integer> seq) {
+
+        return 0;
+    }
+}
