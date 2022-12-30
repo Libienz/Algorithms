565dd92784c6dc25606e05a22a5ac18a25e0705f
Kuen Hee Lee <Leekh10195@gmail.com>
2022-12-30 18:19:15 +0900

    03-04 done

--- Inflearn_Algorithms/src/algorithm_ex/two_pointer/ContinuousNum.java
+++ Inflearn_Algorithms/src/algorithm_ex/two_pointer/ContinuousNum.java
@@ -1,38 +1,41 @@
 package algorithm_ex.two_pointer;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ContinuousNum {
 
-    public static int count(int size, int obj_num, int[] arr) {
-        //어디서 시작하는지를 경우의 수로 나누자...
-        //투포인터를 써서 rt를 증가시키고 lt를 쫓아오게 만들면 모든 케이스에 대한 검증이 가능 한지도 의문인데 ..?
-        int sum = 0;
-        int count= 0;
-        for (int i = 0; i<size; i++) {
-            for(int j = i; j<size; j++) {
-                if(sum<obj_num) {
-                    sum += arr[j];
-                    continue;
-                }
-                break;
-            }
-            if(sum==obj_num) {
-                count++;
-            }
-            sum = 0;
-        }
-        return count;
-    }
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
         int size = sc.nextInt();
-        int obj_num = sc.nextInt();
-        int[] arr = new int[size];
-        for (int i = 0; i<size; i++) {
-            arr[i] = sc.nextInt();
+        int objSum = sc.nextInt();
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        for (int i = 0; i < size; i++) {
+            arr.add(sc.nextInt());
         }
-        System.out.println(count(size, obj_num, arr));
+
+
+        int cnt = 0;
+        //가능한 부분 수열을 구할 때 case 나누는 것을 시작 지점으로 나눈다.
+        //즉 0번째 인덱스에서 시작해서 나올 수 있는 부분수열이 가능한지 따지고
+        //1번째 인덱스에서 시작해서 나올 수 있는 부분수열이 가능한지 따지는 순으로 쭉쭉 나간다.
+        for (int i = 0; i < size; i++) {
+            int subSum = 0;
+            for (int j = i; j < size; j++) {
+                subSum += arr.get(j);
+                if (subSum == objSum) {
+                    cnt++;
+                    break;
+                } else if (subSum > objSum) {
+                    break;
+                }
+
+            }
+        }
+
+        System.out.println(cnt);
+
     }
 }
