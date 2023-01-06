c54b1dfb1b0b6b11c8fb698677c28b6196f24e7e
Kuen Hee Lee <Leekh10195@gmail.com>
2023-01-06 17:29:22 +0900

    05-06 done

--- Inflearn_Algorithms/src/algorithm_ex/stack_queue/SaveDeer.java
+++ Inflearn_Algorithms/src/algorithm_ex/stack_queue/SaveDeer.java
@@ -1,52 +1,29 @@
 package algorithm_ex.stack_queue;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class SaveDeer {
 
-    public static int chosenPrince(int num, int k) {
-
-
-        int left_prince = num;
-        int[] prince = new int[num + 1];
-        prince[0] = 0;
-        int count = 0;
-        for (int i = 1; i<prince.length; i++) {
-            prince[i] = 1;
-        }
-        while(left_prince > 1) {
-            for(int i = 1; i<prince.length; i++) {
-                if (prince[i] == 0) {
-                    continue;
-                }
-                else {
-                    count++;
-                    if (count ==k) {
-                        prince[i] = 0;
-                        count = 0;
-                        left_prince--;
-                    }
-                }
-            }
-
-
-        }
-
-        for (int i = 1; i<prince.length+1; i++) {
-            if (prince[i] == 1) {
-                return i;
-            };
-        }
-        return -1;
-    }
-
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
-        int k = sc.nextInt();
+        Queue<Integer> q = new LinkedList<>();
+        int N = sc.nextInt();
+        int K = sc.nextInt();
 
+        for (int i = 1; i <= N; i++) {
+            q.add(i);
+        }
 
-        System.out.println(chosenPrince(num, k));
+        while (q.size() != 1) {
+            for (int i = 0; i < K-1; i++) {
+                q.add(q.poll());
+            }
+            q.poll();
+
+        }
+        System.out.println(q.poll());
+
     }
 }
