1209654c2dcf93c3e29cae1fbf033cd2acf92065
Kuen Hee Lee <Leekh10195@gmail.com>
2023-02-05 12:39:10 +0900

    01-01 done

--- Inflearn_Algorithms/src/algorithm_ex/about_string/FindChar.java
+++ Inflearn_Algorithms/src/algorithm_ex/about_string/FindChar.java
@@ -4,24 +4,24 @@ import java.util.Scanner;
 
 
 public class FindChar {
-    public static void main (String[] args) {
-
-        String str;
-        char c;
-        int count = 0;
-
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        str = sc.next(); //sc.next()는 공백이 발생하기 전까지의 입력 가져온다. enter 전까지 가져오고 싶으면 sc.nextLine()
-        c = sc.next().charAt(0); //String의 메소드 charAt()
-        str = str.toLowerCase(); //String의 메소드 toLowerCase()
-        for (int i = 0; i<str.length(); i++) {
-            if (str.charAt(i) == Character.toLowerCase(c)) { // Character의 static method
-                count++;
+        String s = sc.next();
+//        System.out.println("s = " + s);
+        char c = sc.next().charAt(0);
+        c = Character.toLowerCase(c);
+//        System.out.println("c = " + c);
+
+        s = s.toLowerCase();
+        char[] str = s.toCharArray();
+        int cnt = 0;
+        for (char cIns : str) {
+            if (cIns == c) {
+                cnt += 1;
             }
         }
 
-        System.out.println(count);
-
+        System.out.println(cnt);
 
     }
 }
