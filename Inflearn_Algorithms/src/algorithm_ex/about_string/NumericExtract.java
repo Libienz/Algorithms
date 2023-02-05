a6ba1690742f44da69492ea3ff28ea135a75940d
Kuen Hee Lee <Leekh10195@gmail.com>
2023-02-05 15:58:54 +0900

    01-09 done

--- Inflearn_Algorithms/src/algorithm_ex/about_string/NumericExtract.java
+++ Inflearn_Algorithms/src/algorithm_ex/about_string/NumericExtract.java
@@ -7,20 +7,10 @@ public class NumericExtract {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        String str = sc.next();
-        //0-9가 아닌 것들 전부 ""로 replace
-        //정규식 이용한 것임
-        //str = str.replaceAll("[^0-9]", "");
-        //System.out.println(Integer.valueOf(str));
-
-        int answer = 0;
-        char[] chars = str.toCharArray();
-        for (char c : chars) {
-            if (Character.isDigit(c)) {
-                answer = (answer * 10) + (c - 48);
-            }
-        }
-        System.out.println(answer);
+        String s = sc.nextLine();
+        s = s.replaceAll("[^0-9]", "");
+        Integer ans = Integer.valueOf(s);
+        System.out.println(ans);
 
     }
 }
