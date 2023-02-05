be85a1928a2972b04c4737434646b0461cbb21c8
Kuen Hee Lee <Leekh10195@gmail.com>
2023-02-05 12:57:10 +0900

    01-03 done

--- Inflearn_Algorithms/src/algorithm_ex/about_string/LongestWord.java
+++ Inflearn_Algorithms/src/algorithm_ex/about_string/LongestWord.java
@@ -7,25 +7,22 @@ import java.util.Scanner;
 public class LongestWord {
 
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
-        String str;
-        int maxWordLength = 0;
-        String maxWord = null;
+        String s = sc.nextLine();
+        int ml = Integer.MIN_VALUE;
+        String ans = "";
 
-        str = sc.nextLine(); //한 줄 읽어오기 엔터전까지
-        String[] wordList = str.split(" "); //공백문자 기준으로 split! 다만 찍어보니 공백문자가 여러번 이어지면 그것도 다 배열에 포함 시킨다.
+        String[] strs = s.split(" ");
+//        System.out.println("strs = " + strs);
 
-        for (String s : wordList) {
-            //System.out.println(s);
-            if (s.length() >= maxWordLength) {
-                maxWordLength = s.length();
-                maxWord = s;
+        for (String str : strs) {
+//            System.out.println("str = " + str);
+            if (str.length() > ml) {
+                ml = str.length();
+                ans = str;
             }
         }
 
-        System.out.println(maxWord);
-
+        System.out.println(ans);
     }
-
 }
