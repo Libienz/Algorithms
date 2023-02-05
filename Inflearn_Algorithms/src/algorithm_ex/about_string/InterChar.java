a1f19a0cd7491a59c5801622f7397232a5925a10
Kuen Hee Lee <Leekh10195@gmail.com>
2023-02-05 20:50:16 +0900

    01-10 done

--- Inflearn_Algorithms/src/algorithm_ex/about_string/InterChar.java
+++ Inflearn_Algorithms/src/algorithm_ex/about_string/InterChar.java
@@ -6,45 +6,33 @@ import java.util.Scanner;
 public class InterChar {
 
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
-        char[] str = sc.next().toCharArray();
+        String s = sc.next();
         char c = sc.next().charAt(0);
+        char[] chars = s.toCharArray();
 
-        int lt, rt;
+        int[] distance = new int[s.length()];
+        ArrayList<Integer> targetIdxs = new ArrayList<>();
 
-        //모든 인덱스에 대해 반복
-        for (int i = 0; i < str.length; i++) {
-            int cnt = 0;
-            lt = i;
-            rt = i;
-            //그 자리에 찾는게 있다면 거리는 0
-            if (str[i] == c) {
-                System.out.printf("%d ", cnt);
-                continue;
+        for (int i = 0; i < distance.length; i++) {
+            distance[i] = Integer.MAX_VALUE;
+        }
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] == c) {
+                targetIdxs.add(i);
             }
-            //그렇지 않다면 반복문 진입
-            //먼저 왼쪽 오른쪽으로 한칸씩 옮겨서 찾는 문자가 있는지 확인해본다.
-            //왼쪽이든 오른쪽이든 먼저 발견하면 그게 최솟값 출력하고 다음 루프로!
-            while (true) {
-                cnt++;
-                //인덱스가 유효하다면 왼쪽으로 한칸 옮겨서 우리가 찾는 문자가 있는지 확인해본다.
-                if (lt-cnt >= 0 && lt-cnt < str.length) {
-                    if (str[lt-cnt] == c) {
-                        System.out.printf("%d ",cnt);
-                        break;
-                    }
+        }
+
+        for (int i = 0; i < distance.length; i++) {
+            for (Integer ti : targetIdxs) {
+                if (distance[i] > Math.abs(ti - i)) {
+                    distance[i] = Math.abs(ti - i);
                 }
-
-                //인덱스가 유효하다면 오른쪽으로 한칸 옮겨서 우리가 찾는 문자가 있는지 확인해본다.
-                if (rt + cnt >= 0 && rt + cnt < str.length) {
-                    if (str[rt + cnt] == c) {
-                        System.out.printf("%d ", cnt);
-                        break;
-
-                    }
-                }
-
             }
+        }
+        for (int d : distance) {
+            System.out.printf("%d ", d);
         }
 
     }
