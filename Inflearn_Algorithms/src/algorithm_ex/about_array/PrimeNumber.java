c9f5f89b790fa5def192d6c8c656175a356b9b6e
Kuen Hee Lee <Leekh10195@gmail.com>
2023-02-11 20:40:17 +0900

    02-05 done

--- Inflearn_Algorithms/src/algorithm_ex/about_array/PrimeNumber.java
+++ Inflearn_Algorithms/src/algorithm_ex/about_array/PrimeNumber.java
@@ -3,32 +3,54 @@ package algorithm_ex.about_array;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+/**
+ * 소수를 구하는 메서드가 필요없다
+ * 제일 작은 소수를 알고 있다면 지워나가는 것이 에라스토테네스의 체 핵심
+ */
 public class PrimeNumber {
 
+
+//    public static boolean isPrime(int num) {
+//        if (num < 2) {
+//            return false;
+//        }
+//        if (num == 2) {
+//            return true;
+//        }
+//        for (int i = 2; i <= num / 2; i++) {
+//            if (num % i == 0) {
+//                return false;
+//            }
+//        }
+//        return true;
+//    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
-        int cnt = 0;
-        ArrayList<Boolean> eras = new ArrayList<>();
+        int n = sc.nextInt(); //1부터 n까지의 소수를 모두 찾아라
 
-        //에라토스테네스 채 초기화
-        eras.add(false);
-        eras.add(false);
-        for (int i = 2; i <= num; i++) {
-            eras.add(true);
+        //에라스토스테네스 체
+        boolean[] eras = new boolean[n + 1];
+
+        //eras 초기화
+        for (int i = 0; i < eras.length; i++) {
+            eras[i] = true;
         }
 
-        for (int i = 0; i < eras.size(); i++) {
-            if (eras.get(i)) {
-                //소수의 배수들은 소수가 아님 걸러낸다!
-                for (int j = i*2; j <= eras.size(); j += i) {
-                    eras.set(j, false);
+        for (int i =2; i <= n; i++) {
+            if (eras[i] == true) {
+                for (int j = i + i; j <= n; j = j + i) {
+                    eras[j] = false;
                 }
+            } else {
+                eras[i] = false;
+                continue;
             }
         }
 
-        for (int i = 0; i < eras.size(); i++) {
-            if (eras.get(i)) {
+        int cnt = 0;
+        for (int i = 2; i <= n; i++) {
+            if (eras[i] == true) {
+//                System.out.println("??");
                 cnt++;
             }
         }
