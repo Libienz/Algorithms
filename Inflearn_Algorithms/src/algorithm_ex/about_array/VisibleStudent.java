3dca30ea4bc95d5b5dddbd676e750a73ecc12e8a
Kuen Hee Lee <Leekh10195@gmail.com>
2023-02-11 18:05:57 +0900

    02-02 done

--- Inflearn_Algorithms/src/algorithm_ex/about_array/VisibleStudent.java
+++ Inflearn_Algorithms/src/algorithm_ex/about_array/VisibleStudent.java
@@ -8,22 +8,23 @@ import java.util.Scanner;
 public class VisibleStudent {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int cnt = sc.nextInt();
-        int res = 0; //보이는 학생 수
-        int subMax = 0;
 
-        ArrayList<Integer> arr = new ArrayList<>();
-        arr.add(sc.nextInt());
-        subMax = arr.get(0); //맨 앞은 항상 보임 subMax 갱신
-        res++; //맨 앞은 항상 보임
-        for (int i = 1; i < cnt; i++) {
-            arr.add(sc.nextInt());
-            if (subMax < arr.get(i)) { //현재 보는 학생이 subMax보다 크다면
-                subMax = arr.get(i); //subMax갱신
-                res++; //보이는 학생 ++
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(); //n명의 학생
+        int[] arr = new int[n];
+        int prevMax = Integer.MIN_VALUE;
+        int res = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+            if (arr[i] > prevMax) {
+                res++;
+                prevMax = arr[i];
             }
         }
+
         System.out.println(res);
+
+
     }
 }
