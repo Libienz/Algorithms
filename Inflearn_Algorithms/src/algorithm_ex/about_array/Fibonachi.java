16b7361ed06712b9fb98b84944b9f3f12cc02491
Kuen Hee Lee <Leekh10195@gmail.com>
2023-02-11 18:18:59 +0900

    02-04 done

--- Inflearn_Algorithms/src/algorithm_ex/about_array/Fibonachi.java
+++ Inflearn_Algorithms/src/algorithm_ex/about_array/Fibonachi.java
@@ -4,19 +4,23 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Fibonachi {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(); //총 항수
 
-        int cnt = sc.nextInt();
-        ArrayList<Integer> fib = new ArrayList<>();
-        fib.add(1);
-        fib.add(1);
-        for (int i = 2; i < cnt; i++) {
-            fib.add(fib.get(i - 1) + fib.get(i - 2));
+        int[] fib = new int[n];
+        fib[0] = 1;
+        fib[1] = 1;
+
+        for (int i = 0; i < n; i++) {
+            if (i == 0 || i == 1) {
+                System.out.print("1 ");
+                continue;
+            }
+            fib[i] = fib[i - 1] + fib[i - 2];
+            System.out.print(fib[i] + " ");
         }
 
-        for (int n : fib) {
-            System.out.printf("%d ", n);
-        }
     }
 }
