ead29a16cdcf7e7f0f454999f106d18a8b64fa7e
Kuen Hee Lee <Leekh10195@gmail.com>
2023-02-11 22:45:19 +0900

    02-08 done

--- Inflearn_Algorithms/src/algorithm_ex/about_array/Ranking.java
+++ Inflearn_Algorithms/src/algorithm_ex/about_array/Ranking.java
@@ -7,25 +7,26 @@ public class Ranking {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int cnt = sc.nextInt();
+        int n = sc.nextInt(); //n명의 학생이 있다.
+        ArrayList<Integer> scores = new ArrayList<>();
 
-        ArrayList<Integer> arr = new ArrayList<>();
-        for (int i = 0; i < cnt; i++) {
-            arr.add(sc.nextInt());
+        for (int i = 0; i < n; i++) {
+            scores.add(sc.nextInt());
         }
 
-        for (int i = 0; i < cnt; i++) {
-            int score = arr.get(i);
+        for (int i = 0; i < scores.size(); i++) {
+            Integer score = scores.get(i);
+            //i 번째 학생의 점수는 score
             int rank = 1;
-            for (int j = 0; j < cnt; j++) {
+            for (int j = 0; j < scores.size(); j++) {
                 if (i == j) {
                     continue;
                 }
-                if (score < arr.get(j)) {
+                if (scores.get(j) > score) {
                     rank++;
                 }
             }
-            System.out.print(rank + " ");
+            System.out.print(rank +" ");
         }
     }
 
