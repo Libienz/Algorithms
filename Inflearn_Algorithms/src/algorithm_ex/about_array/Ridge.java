d2e422ac075361c9d21eea186c5b93e575859815
Kuen Hee Lee <Leekh10195@gmail.com>
2023-02-19 12:30:42 +0900

    02-10 done

--- Inflearn_Algorithms/src/algorithm_ex/about_array/Ridge.java
+++ Inflearn_Algorithms/src/algorithm_ex/about_array/Ridge.java
@@ -4,32 +4,41 @@ import java.util.Scanner;
 
 public class Ridge {
 
+    static int[][] map;
+
+    private static boolean isRidge(int r, int c) {
+        int h = map[r][c];
+        int u = map[r - 1][c];
+        int d = map[r + 1][c];
+        int l = map[r][c - 1];
+        int rg = map[r][c + 1];
+        if (h > u && h > d && h > l && h > rg) {
+            return true;
+        }
+        return false;
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int size = sc.nextInt()+2;
-        int[][] map = new int[size][size];
-        int cnt = 0;
+        int n = sc.nextInt();
 
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                if (i == 0 || j == 0 || i == size - 1 || j == size - 1) {
-                    map[i][j] = 0;
-                    continue;
-                }
+        //n+2 by n+2 격자 생성
+        map = new int[n + 2][n + 2];
+
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < n + 1; j++) {
                 map[i][j] = sc.nextInt();
             }
         }
-        for (int i = 1; i < size-1; i++) {
-            for (int j = 1; j < size-1; j++) {
-                if (map[i][j] > map[i - 1][j] && map[i][j] > map[i][j - 1] && map[i][j] > map[i + 1][j] && map[i][j] > map[i][j + 1]) {
+
+        int cnt = 0;
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < n + 1; j++) {
+                if (isRidge(i, j)) {
                     cnt++;
                 }
             }
         }
-
         System.out.println(cnt);
-
-
 
     }
 }
