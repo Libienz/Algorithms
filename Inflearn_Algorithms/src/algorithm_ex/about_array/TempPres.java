5dbf17ccea7a73d219ca7b7c8164969bdaf21fc6
Kuen Hee Lee <Leekh10195@gmail.com>
2023-02-19 12:58:24 +0900

    02-11 done

--- Inflearn_Algorithms/src/algorithm_ex/about_array/TempPres.java
+++ Inflearn_Algorithms/src/algorithm_ex/about_array/TempPres.java
@@ -6,39 +6,73 @@ import java.util.Scanner;
 
 public class TempPres {
 
+    private static boolean everSame(int s1, int s2, int[][] ca) {
+
+        for (int i = 0; i < 5; i++) {
+            int c1 = ca[s1][i]; //s1학생이 i학년에 c1반이었음
+            int c2 = ca[s2][i]; //s2학생이 i학년에 c2반이었음
+
+            if (c1 == c2) {
+                return true;
+            }
+
+        }
+        return false;
+
+
+
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int size = sc.nextInt();
-        int[][] arr = new int[size][5];
-        int cmax = 0;
-        int pres = 1;
-        for (int i = 0; i < size; i++) {
+        int s = sc.nextInt(); //학생 수
+        //class arr 학생들이 특정학년에 몇반이었는지 알 수 있는 배열
+        //학생들은 row로 학년은 col로 구분
+        int[][] ca = new int[s][5];
+        for (int i = 0; i < s; i++) {
             for (int j = 0; j < 5; j++) {
-                arr[i][j] = sc.nextInt();
+                ca[i][j] = sc.nextInt();
             }
         }
 
-        //i학생과 j학생은 한번이라도 같은 반이었던 적이 있는가?
-        for (int i = 0; i < size; i++) {
-            int cnt = 0;
-            for (int j = 0; j < size; j++) {
+        //relation -> i와 j학생이 같은 반이었던적이 있는지 알려주는 배열
+        boolean[][] rl = new boolean[s][s];
+        for (int i = 0; i < s; i++) {
+            for (int j = 0; j < s; j++) {
                 if (i == j) {
                     continue;
                 }
-                for (int k = 0; k < 5; k++) {
-                    if (arr[i][k] == arr[j][k]) {
-                        cnt++;
-                        break;
-                    }
+                //i학생과 j학생은 같은 반이었던 적이 있는가?
+                if (everSame(i, j, ca)) {
+                    rl[i][j] = true;
                 }
             }
-            if (cnt > cmax) {
-                cmax = cnt;
-                pres = i+1;
-            }
-
         }
-        System.out.println(pres);
 
+        int max = Integer.MIN_VALUE;
+        int tp = 0;
+        for (int i = 0; i < rl.length; i++) {
+            //i학생과 한번이라도 같은 반이었던 학생수
+            int cnt = 0;
+            for (int j = 0; j < rl[0].length; j++) {
+                if (i == j) {
+                    continue;
+                }
+                if (rl[i][j]) {
+                    cnt++;
+                }
+            }
+            if (cnt > max) {
+                max = cnt;
+                tp = i + 1;
+            }
+        }
+
+        System.out.println(tp);
+//        for (int i = 0; i < rl.length; i++) {
+//            for (int j = 0; j < rl.length; j++) {
+//                System.out.print(rl[i][j]+ " ");
+//            }
+//            System.out.println();
+//        }
     }
 }
