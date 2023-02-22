8ed82695c415662c931e89ac34d1fb71e904035b
Kuen Hee Lee <Leekh10195@gmail.com>
2023-02-22 14:52:11 +0900

    04-02 done

--- Inflearn_Algorithms/src/algorithm_ex/hashmap_treeset/Anagram.java
+++ Inflearn_Algorithms/src/algorithm_ex/hashmap_treeset/Anagram.java
@@ -3,6 +3,7 @@ package algorithm_ex.hashmap_treeset;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Anagram {
 
@@ -11,21 +12,37 @@ public class Anagram {
 
         String s1 = sc.next();
         String s2 = sc.next();
-        Map<Character, Integer> map = new HashMap<>();
-        for (int i = 0; i < s1.length(); i++) {
-            map.put(s1.charAt(i), map.getOrDefault(s1.charAt(i), 0) + 1);
-        }
-        for (int i = 0; i < s2.length(); i++) {
-            map.put(s2.charAt(i), map.getOrDefault(s2.charAt(i),0) - 1);
+
+        if (isAnagram(s1, s2)) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
         }
 
-        for (Character c : map.keySet()) {
+    }
+
+    public static boolean isAnagram(String s1, String s2) {
+
+        Map<Character, Integer> map = new HashMap<>();
+
+        //s1의 구성 정보를 map에 담는다.
+        char[] s1arr = s1.toCharArray();
+        for (char c : s1arr) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        //s2의 구성 정보를 map과 대조한다. -> s2의 정보를 map에서 빼서 후에 map의 모든 key에 대한 val이 0인지 확인
+        char[] s2arr = s2.toCharArray();
+        for (char c : s2arr) {
+            map.put(c, map.getOrDefault(c, 0) - 1);
+        }
+
+        Set<Character> set = map.keySet();
+        for (Character c : set) {
             if (map.get(c) != 0) {
-                System.out.println("NO");
-                return;
+                return false;
             }
         }
-        System.out.println("YES");
+        return true;
 
     }
 }
