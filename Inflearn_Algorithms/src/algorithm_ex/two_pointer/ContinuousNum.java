a11557364c68ed0f28048d7f498bb4b74193a9c2
Kuen Hee Lee <Leekh10195@gmail.com>
2023-02-22 10:39:49 +0900

    03-04 done

--- Inflearn_Algorithms/src/algorithm_ex/two_pointer/ContinuousNum.java
+++ Inflearn_Algorithms/src/algorithm_ex/two_pointer/ContinuousNum.java
@@ -5,36 +5,36 @@ import java.util.Scanner;
 
 public class ContinuousNum {
 
+
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
-        int size = sc.nextInt();
-        int objSum = sc.nextInt();
-        ArrayList<Integer> arr = new ArrayList<>();
+        int n = sc.nextInt(); //n크기의 수열이 주어졌을 때
+        int m = sc.nextInt(); //수열의 부분수열 중 합이 m이 되는 경우의 수를 찾아라
 
-        for (int i = 0; i < size; i++) {
+        //주어진 수열을 배열에 담는다.
+        ArrayList<Integer> arr = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
             arr.add(sc.nextInt());
         }
 
-
+        //case 분류를 부분수열의 시작위치로
+        //i에서 시작하는 부분 수열 중 합이 m이 되는 수열이 있는지 체크
         int cnt = 0;
-        //가능한 부분 수열을 구할 때 case 나누는 것을 시작 지점으로 나눈다.
-        //즉 0번째 인덱스에서 시작해서 나올 수 있는 부분수열이 가능한지 따지고
-        //1번째 인덱스에서 시작해서 나올 수 있는 부분수열이 가능한지 따지는 순으로 쭉쭉 나간다.
-        for (int i = 0; i < size; i++) {
-            int subSum = 0;
-            for (int j = i; j < size; j++) {
-                subSum += arr.get(j);
-                if (subSum == objSum) {
-                    cnt++;
+        for (int i = 0; i < n; i++) {
+            int ss = 0; //subsum
+            for (int j = i; j < n; j++) {
+                ss += arr.get(j);
+                if (ss > m) {
                     break;
-                } else if (subSum > objSum) {
+                }
+                if (ss == m) {
+                    cnt++;
                     break;
                 }
 
             }
-        }
 
+        }
         System.out.println(cnt);
 
     }
