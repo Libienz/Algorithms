44feeb39072abc5383f5c1a351680b1d0346d9f8
Kuen Hee Lee <Leekh10195@gmail.com>
2023-02-22 10:46:07 +0900

    03-05 done

--- Inflearn_Algorithms/src/algorithm_ex/two_pointer/ContinuousNumSum.java
+++ Inflearn_Algorithms/src/algorithm_ex/two_pointer/ContinuousNumSum.java
@@ -5,21 +5,20 @@ import java.util.Scanner;
 public class ContinuousNumSum {
 
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
-        int objNum = sc.nextInt();
+        int n = sc.nextInt(); //2개 이상의 연속된 자연수의 합으로 정수 N을 표현하는 방법의 가짓수를 구하라
 
+        //n보다 작은 연속된 자연수들의 합으로 n을 구성하기 위한 모든 case를 시작숫자로 분류한다.
         int cnt = 0;
-
-        for (int i = 1; i <= objNum / 2; i++) {
-            int subSum = 0;
-            for (int j = i; j < objNum; j++) {
-                subSum += j;
-                if (subSum == objNum) {
-                    cnt++;
+        for (int i = 1; i <= n/2; i++) {
+            //i부터 연속된 자연수들을 더해가며 합이 N을 만들 수 있는지 확인한다.
+            int ss = 0; //subsum
+            for (int j = i; j <= n; j++) {
+                ss += j;
+                if (ss > n) {
                     break;
-                }
-                if (subSum > objNum) {
+                } else if (ss == n) {
+                    cnt++;
                     break;
                 }
             }
