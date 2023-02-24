cce72797ee6f1406ea538b27c7e52bc188561ae7
Kuen Hee Lee <Leekh10195@gmail.com>
2023-02-24 12:03:26 +0900

    for CT

--- Inflearn_Algorithms/src/algorithm_ex/dfs_bfs/EqualSumSubset.java
+++ Inflearn_Algorithms/src/algorithm_ex/dfs_bfs/EqualSumSubset.java
@@ -6,63 +6,61 @@ import java.util.Scanner;
 public class EqualSumSubset {
 
     static int size;
-    static boolean isSameSum = false;
-    static ArrayList<Integer> arr = new ArrayList<>();
-    static ArrayList<Integer> sums = new ArrayList<>();
-    static ArrayList<Integer> subSet1 = new ArrayList<>();
-    static ArrayList<Integer> subSet2 = new ArrayList<>();
-
+    static ArrayList<Integer> set;
+    static ArrayList<Integer> subset1;
+    static ArrayList<Integer> subset2;
+    static boolean isExist = false;
 
     public static void dfs(int idx) {
+//        System.out.println("idx = " + idx);
         if (idx >= size) {
-
-            for (Integer num : arr) {
-                if (!subSet1.contains(num)) {
-                    subSet2.add(num);
-                }
-            }
-//            System.out.println("subSet1 = " + subSet1);
-//            System.out.println("subSet2 = " + subSet2);
+//            System.out.println("idx = " + idx);
+//            System.out.println("subset1 = " + subset1);
+//            System.out.println("subset2 = " + subset2);
             int sum1 = 0;
             int sum2 = 0;
-            for (Integer num : subSet1) {
+            for (Integer num : subset1) {
                 sum1 += num;
             }
-            for (Integer num : subSet2) {
+            for (Integer num : subset2) {
                 sum2 += num;
             }
             if (sum1 == sum2) {
-                isSameSum = true;
+                isExist = true;
             }
-            subSet2.clear();
+//            System.out.println("subset1 = " + subset1);
+//            System.out.println("subset2 = " + subset2);
             return;
+//            System.out.println("YES");
+        } else {
+            int cur = set.get(idx);
+            subset1.add(cur);
+            dfs(idx + 1);
+            subset1.remove((Object) cur);
+            subset2.add(cur);
+            dfs(idx + 1);
+            subset2.remove((Object) cur);
         }
-        //쓴다
-        subSet1.add(arr.get(idx));
-        dfs(idx + 1);
-        //쓰지 않는다.
-        subSet1.remove((Object) arr.get(idx));
-        dfs(idx + 1);
     }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         size = sc.nextInt();
+        set = new ArrayList<>();
+        subset1 = new ArrayList<>();
+        subset2 = new ArrayList<>();
         for (int i = 0; i < size; i++) {
-            arr.add(sc.nextInt());
+            set.add(sc.nextInt());
         }
 
         dfs(0);
-        if (isSameSum) {
+        if (isExist) {
             System.out.println("YES");
+
         } else {
             System.out.println("NO");
         }
 
-//        System.out.println("sums = " + sums);
 
-//        System.out.println("sums = " + sums.size());
-//        System.out.println("sums = " + sums);
 
     }
 }
