9e8e7eb6c2cd49a817ef9fdd163801715ee7c642
Kuen Hee Lee <Leekh10195@gmail.com>
2023-02-24 21:24:50 +0900

    for CT

--- Inflearn_Algorithms/src/algorithm_ex/greedy/FriendUF.java
+++ Inflearn_Algorithms/src/algorithm_ex/greedy/FriendUF.java
@@ -1,67 +1,67 @@
 package algorithm_ex.greedy;
 
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class FriendUF {
 
+    public static int[] arr;
 
-    //unf: 인덱스 1의 값이 3이라면 1번 학생은 3그룹에 속한다는 뜻
-    static int unf[];
-
-    public static void union(int s1, int s2) {
-
-        int g1 = find(s1);
-        int g2 = find(s2);
-
-        //s1이 속한 그룹번호 g1과 s2가 속한 그룹번호 g2가 다를 경우 두 그룹을 합친다.
-        //find는 파라미터로 넘어간 학생이 속한 그룹 번호를 알려주는 메서드
-        if (g1 != g2) {
-            unf[g1] = g2;
+    //그룹 번호 리턴
+    public static int find(int p) {
+        if (arr[p] == p) {
+            return p;
+        } else {
+            return find(arr[p]);
         }
     }
 
-    //파라미터로 넘어온 학생이 (학생번호) 어느 그룹에 속하는지를 알리는 메서드
-    public static int find(int s) {
-        if (s == unf[s]) {
-            return s;
-        } else {
-            //return find(unf[s]);
-            //위의 코드도 정상동작 but 경로 압축한 버젼은 다음과 같다
-            return unf[s] = find(unf[s]);
-
+    public static void union(int p1, int p2) {
+        int g1 = find(p1);
+        int g2 = find(p2);
+        if (g1 != g2) {
+            if (g1 > g2) {
+                arr[g1] = g2;
+            } else {
+                arr[g2] = g1;
+            }
         }
-
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int s = sc.nextInt();  //학생 명수 (student)
-        int r = sc.nextInt(); //숫자 쌍의 개수 (relation)
+        int n = sc.nextInt(); //학생 수
+        int m = sc.nextInt(); //숫자 쌍의 개수
+        arr = new int[n + 1];
 
-        unf = new int[s + 1];
-
-        for (int i = 0; i <= s; i++) {
-            unf[i] = i;
+        for (int i = 0; i < n + 1; i++) {
+            arr[i] = i;
         }
 
-        for (int i = 0; i < r; i++) {
-            int s1 = sc.nextInt();
-            int s2 = sc.nextInt();
-            union(s1, s2);
+        for (int i = 0; i < m; i++) {
+            int p1 = sc.nextInt();
+            int p2 = sc.nextInt();
+
+            //p1과 p2는 friend
+            union(p1, p2);
         }
 
-        int s1 = sc.nextInt();
-        int s2 = sc.nextInt();
-        //문제에서는 s1과 s2가 친구사이인지 확인하고 싶다.
-        if (find(s1) == find(s2)) {
+        int f1 = sc.nextInt();
+        int f2 = sc.nextInt();
+//        System.out.println("f1 = " + f1);
+//        System.out.println("f2 = " + f2);
+        if (find(f1) == find(f2)) {
             System.out.println("YES");
         } else {
             System.out.println("NO");
+
         }
-//        for (int i : unf) {
-//            System.out.println(i);
+//        for (int num : arr) {
+//            System.out.println(num);
+//
 //        }
+
     }
 
 }
