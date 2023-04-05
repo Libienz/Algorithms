eed948266c85eb37d7ec2c9d64bb930f0223150e
Lkh-dev <leekh10195@gmail.com>
2023-04-05 13:21:43 +0900

    05-01 done

--- Inflearn_Algorithms/src/algorithm_ex/stack_queue/BraceCheck.java
+++ Inflearn_Algorithms/src/algorithm_ex/stack_queue/BraceCheck.java
@@ -1,33 +1,44 @@
 package algorithm_ex.stack_queue;
 
-import java.util.Scanner;
-import java.util.Stack;
+import java.util.*;
 
 public class BraceCheck {
+
+    public static boolean braceCheck(String braces) {
+        Stack<Character> stack = new Stack<>();
+        char[] ba = braces.toCharArray();
+        for (Character c : ba) {
+            //여는 괄호라면 스택에 넣는다.
+            if (c == '(') {
+                stack.push(c);
+            }
+            //닫는 괄호라면 스택에서 하나를 팝한다.
+            else if (c == ')') {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                else {
+                    stack.pop();
+                }
+
+            }
+        }
+        if (stack.isEmpty()) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String braces = sc.next();
 
-        Stack<Character> stack = new Stack<>();
-        for (int i = 0; i < braces.length(); i++) {
-            if (braces.charAt(i) == '(') {
-                stack.push('(');
-            }
-            else {
-                if (stack.empty()) {
-                    System.out.println("NO");
-                    return;
-                } else {
-                    stack.pop();
-                }
-            }
-        }
-        if (stack.empty()) {
+        if (braceCheck(braces)) {
             System.out.println("YES");
         } else {
             System.out.println("NO");
         }
-
     }
 }
