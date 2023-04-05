f9a6a95aa91ac49f452781dd55e8ccfff2fd53b9
Lkh-dev <leekh10195@gmail.com>
2023-04-05 13:46:22 +0900

    05-03 in progress..

--- Inflearn_Algorithms/src/algorithm_ex/stack_queue/PuppetDraw.java
+++ Inflearn_Algorithms/src/algorithm_ex/stack_queue/PuppetDraw.java
@@ -1,69 +1,57 @@
 package algorithm_ex.stack_queue;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
-import java.util.Stack;
+import java.util.*;
 
 public class PuppetDraw {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int size = sc.nextInt();
-        int[][] board = new int[size][size];
-        int cnt = 0;
 
-        //보드 초기화
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] board = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
                 board[i][j] = sc.nextInt();
             }
         }
-
-        //moves 배열 초기화
-        int mvSize = sc.nextInt();
-        int[] moves = new int[mvSize];
-        for (int i = 0; i < mvSize; i++) {
-            moves[i] = sc.nextInt() - 1;
+        int mvcount = sc.nextInt();
+//        System.out.println("mvcount = " + mvcount);
+        int[] mvs = new int[mvcount];
+        for (int i = 0; i < mvcount; i++) {
+            mvs[i] = sc.nextInt();
         }
 
-        //바구니
-        Stack<Integer> bucket = new Stack<>();
-
-        //move 배열의 내용에 따라 하나씩 바구니에 옮긴다.
-        for (int move : moves) {
-
-            //크레인이 0이 아닌 인형을 발견할 때까지 내려간다.
-            for (int i = 0; i < size; i++) {
-                if (board[i][move] == 0) {
+        Stack<Integer> stack = new Stack<>();
+        int res = 0;
+        for (int i : mvs) {
+            //i번째 레인을 바구니에 옮길 것임
+//            System.out.println("i = " + i);
+            for (int j = 0; j<n; j++) {
+                if (board[j][i - 1] == 0) {
                     continue;
-                }
-                //여기로 왔다는 것은 0이 아닌 것을 발견하였다는 것 버켓에 넣을 것임
-                //버켓에 들어가게 될 인형의 종류: type
-                int type = board[i][move];
-                board[i][move] = 0;
+                } else {
+                    int obj = board[j][i-1];
+                    board[j][i-1] = 0;
+                    if (stack.isEmpty()) {
+                        stack.push(obj);
+                    }
 
-                //버켓이 비어있거나 버켓에 맨 위에 담겨있는 인형의 종류가 내가 넣으려는 인형의 종류와 다르다면
-                //그냥 넣으면 된다.
-                if (bucket.empty() || bucket.peek() != type) {
-                    bucket.push(type);
-                    //break해야 된다.. 반복문 돌지 않도록! 이런 부분 빠릿하게 생각해낼 수 있어야..
-                    break;
-                } else if (bucket.peek() == type) {
-                    //넣으려는 인형이 꼭대기에 있는 인형과 일치한다면
-                    //pop하고 두개가 없어졌음을 명시
-                    bucket.pop();
-                    cnt = cnt + 2;
-                    break;
-                }
+                    else {
+                        if (obj == stack.peek()) {
+                            stack.pop();
+                            res += 2;
+                        }
+                        else {
+                            stack.push(obj);
+                        }
 
+                    }
+
+                }
             }
+
         }
-        System.out.println(cnt);
-
-
-
-
-
+        System.out.println(res);
     }
 }
