bc5d7c56c8ce1a2b9eaa824b794ecbdbb6def67c
Kuen Hee Lee <Leekh10195@gmail.com>
2023-04-11 22:13:13 +0900

    q2583 in progress..

---
+++ boj/src/Q2583.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Q2583 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("hello");
+    }
+}
