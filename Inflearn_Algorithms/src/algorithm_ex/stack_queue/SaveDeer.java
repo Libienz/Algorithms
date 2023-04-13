d81ba3d5006667ac04169ccbb0bde0ee0b04a72c
Lkh-dev <leekh10195@gmail.com>
2023-04-13 11:19:55 +0900

    05-06 done

--- Inflearn_Algorithms/src/algorithm_ex/stack_queue/SaveDeer.java
+++ Inflearn_Algorithms/src/algorithm_ex/stack_queue/SaveDeer.java
@@ -7,23 +7,26 @@ import java.util.Scanner;
 public class SaveDeer {
 
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
         Queue<Integer> q = new LinkedList<>();
-        int N = sc.nextInt();
-        int K = sc.nextInt();
 
-        for (int i = 1; i <= N; i++) {
+        int pn = sc.nextInt();
+        int k = sc.nextInt(); //k를 외치면 제외되는 왕자
+
+        for (int i = 1; i <= pn; i++) {
             q.add(i);
         }
 
-        while (q.size() != 1) {
-            for (int i = 0; i < K-1; i++) {
-                q.add(q.poll());
+        while (q.size() > 1) {
+            for (int i = 0; i < k-1; i++) {
+                int keep = q.poll();
+                q.add(keep);
             }
             q.poll();
-
         }
-        System.out.println(q.poll());
+        int gp = q.poll();
 
+        System.out.println(gp);
     }
 }
