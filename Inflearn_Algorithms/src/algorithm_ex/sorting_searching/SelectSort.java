eb64dc274de21bd74b80719ef9a7b0e29bce1c2e
Lkh-dev <leekh10195@gmail.com>
2023-04-14 13:54:02 +0900

    06-01 done

--- Inflearn_Algorithms/src/algorithm_ex/sorting_searching/SelectSort.java
+++ Inflearn_Algorithms/src/algorithm_ex/sorting_searching/SelectSort.java
@@ -6,33 +6,47 @@ import java.util.Scanner;
 public class SelectSort {
 
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
         int size = sc.nextInt();
-        ArrayList<Integer> arr = new ArrayList<>();
+        int[] arr = new int[size];
+
         for (int i = 0; i < size; i++) {
-            arr.add(sc.nextInt());
+            arr[i] = sc.nextInt();
         }
 
-        //select sort
-        int idx = 0;
-        while (idx < size) {
+        //선택정렬
+        //최솟값 찾고 0번째 인덱스와 교체
+        //최솟값 찾고 1번째 인덱스와 교체
+        //...
+
+        //최솟값을 size-1만큼 찾아야 함
+        //루프를 마치고 나면 i번째 인덱스까지 정렬이 완료된 상태
+        for (int i = 0; i < size -1; i++) {
+
+            //최솟값 찾기
             int min = Integer.MAX_VALUE;
             int minIdx = -1;
-            //idx부터 끝까지 중에서 가장 작은 놈 찾아서 idx자리에 집어넣을 것
-            for (int i = idx; i < size; i++) {
-                if (arr.get(i) < min) {
-                    min = arr.get(i);
-                    minIdx = i;
+            for (int j = i; j < size; j++) {
+                if (arr[j] < min) {
+                    min = arr[j];
+                    minIdx = j;
                 }
             }
-            Integer tmp = arr.get(idx);
-            arr.set(idx, min);
-            arr.set(minIdx, tmp);
-            idx++;
+
+            //최솟값은 min, 최솟값의 인덱스는 minIdx
+            //arr[i]와 arr[minIdx] 스왑
+
+            int tmp = arr[minIdx];
+            arr[minIdx] = arr[i];
+            arr[i] = tmp;
+            //이로써 배열은 i번째 까지 정렬된 상태가 된다.
+
 
         }
-        for (Integer integer : arr) {
-            System.out.print(integer+" ");
+
+        for (int num : arr) {
+            System.out.print(num + " ");
         }
 
     }
