59557181eec5aae16a9b0ca611478cfa95794ea0
Lkh-dev <leekh10195@gmail.com>
2023-04-14 14:30:49 +0900

    06-04 done

--- Inflearn_Algorithms/src/algorithm_ex/sorting_searching/InsertionSort.java
+++ Inflearn_Algorithms/src/algorithm_ex/sorting_searching/InsertionSort.java
@@ -6,37 +6,31 @@ import java.util.Scanner;
 public class InsertionSort {
 
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
         int size = sc.nextInt();
-        ArrayList<Integer> arr = new ArrayList<>();
+        int[] arr = new int[size];
 
         for (int i = 0; i < size; i++) {
-            arr.add(sc.nextInt());
+            arr[i] = sc.nextInt();
         }
 
-        //i번째 원소를 i번째 이전의 정렬된 배열 중 어디에 꽂아 넣어야 하나를 찾는다.
+        //Selection sort
+        //2번째 원소부터 시작 그 이전은 정렬된 상태로 보는 것 2번째 원소를 적절한 곳에 삽입
         for (int i = 1; i < size; i++) {
-            //System.out.println("arr = " + arr);
-            //cur가 자기보다 왼쪽에 정렬된 애들 중에서 방을 찾는다.
-            int cur = arr.get(i);
-            //System.out.println("cur = " + cur);
-            //들어갈 수 있는 위치는 0부터 i중 하나
-            //왼쪽 중에서 자기가 들어갈 인덱스를 찾는다.
-            int idx = i;
-            while (idx > 0) {
-                if (cur > arr.get(idx - 1)) {
-                    break; //idx에 들어갈 수 있다.
-                } else {
-                    arr.set(idx, arr.get(idx - 1));
-                    idx--;
+            // arr[i]를 왼쪽에 정렬된 부분에 삽입시킬건데 어디에 넣을 지 정해야 함
+            for (int j = i; j >= 1; j--) {
+                //arr[i]가 더 작으면 스왑
+                if (arr[j] < arr[j-1]) {
+                    int tmp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = tmp;
                 }
             }
-            arr.set(idx, cur);
-
         }
 
-        for (Integer integer : arr) {
-            System.out.print(integer + " ");
+        for (int num : arr) {
+            System.out.print(num + " ");
         }
     }
 }
