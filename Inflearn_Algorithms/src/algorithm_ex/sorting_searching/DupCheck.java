ea27269095fb24f246ec9b87b5cf1087e3a766ee
Lkh-dev <leekh10195@gmail.com>
2023-04-15 21:45:19 +0900

    06-05 done

--- Inflearn_Algorithms/src/algorithm_ex/sorting_searching/DupCheck.java
+++ Inflearn_Algorithms/src/algorithm_ex/sorting_searching/DupCheck.java
@@ -1,27 +1,23 @@
 package algorithm_ex.sorting_searching;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Scanner;
+import java.util.*;
 
 public class DupCheck {
 
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
         int size = sc.nextInt();
-        ArrayList<Integer> arr = new ArrayList<>();
+
+        Set<Integer> set = new HashSet<>();
         for (int i = 0; i < size; i++) {
             int num = sc.nextInt();
-            if (arr.contains(num)) {
-                System.out.println("D");
-                return;
-            }
-            arr.add(num);
+            set.add(num);
         }
-        System.out.println("U");
-
-        //배열 정렬
-
+        if (set.size() < size) {
+            System.out.println("D");
+        } else {
+            System.out.println("U");
+        }
     }
 }
