1fdb49e248443361499a8d9b98cc62479fd667fe
Lkh-dev <leekh10195@gmail.com>
2023-04-28 20:42:08 +0900

    07-04 memoi ver

--- Inflearn_Algorithms/src/algorithm_ex/recursive/FibRecursive.java
+++ Inflearn_Algorithms/src/algorithm_ex/recursive/FibRecursive.java
@@ -2,14 +2,21 @@ package algorithm_ex.recursive;
 
 import java.util.*;
 
+
 public class FibRecursive {
 
+    static int[] memoi;
     public static int getFib(int n) {
 
+        if (memoi[n - 1] != 0) {
+            return memoi[n - 1];
+        }
         if (n == 1 || n == 2) {
+            memoi[n - 1] = 1;
             return 1;
         } else {
-            return getFib(n - 2) + getFib(n - 1);
+            memoi[n-1] = getFib(n - 2) + getFib(n - 1);
+            return memoi[n - 1];
         }
 
     }
@@ -17,6 +24,7 @@ public class FibRecursive {
 
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        memoi = new int[n];
 
         for (int i = 1; i <= n; i++) {
             System.out.print(getFib(i) + " ");
