df6d52045cddd93992bbe555cf77c9b059ea45fd
Lkh-dev <leekh10195@gmail.com>
2023-04-28 20:38:59 +0900

    07-04 no memoi done

--- Inflearn_Algorithms/src/algorithm_ex/recursive/FibRecursive.java
+++ Inflearn_Algorithms/src/algorithm_ex/recursive/FibRecursive.java
@@ -1,25 +1,25 @@
 package algorithm_ex.recursive;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class FibRecursive {
 
-    public static int getFib(int num) {
-        if (num == 1 || num == 2) {
+    public static int getFib(int n) {
+
+        if (n == 1 || n == 2) {
             return 1;
         } else {
-            return getFib(num - 1)
-            +getFib(num - 2);
+            return getFib(n - 2) + getFib(n - 1);
         }
+
     }
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
-        for (int i = 1; i <= num; i++) {
-            System.out.print(getFib(i)+" ");
+        int n = sc.nextInt();
 
+        for (int i = 1; i <= n; i++) {
+            System.out.print(getFib(i) + " ");
         }
-
-
     }
 }
