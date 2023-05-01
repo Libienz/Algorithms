ea8fd4919abd5a5f59c8315039702433d32ad700
Lkh-dev <leekh10195@gmail.com>
2023-05-01 13:31:22 +0900

    07-06 done

--- Inflearn_Algorithms/src/algorithm_ex/recursive/Subset.java
+++ Inflearn_Algorithms/src/algorithm_ex/recursive/Subset.java
@@ -4,27 +4,34 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Subset {
+
+    static int[] set;
     static int n;
-    static ArrayList<Integer> arr = new ArrayList<>();
+    static ArrayList<Integer> subSet;
+    public static void dfs(int idx) {
 
-    public static void DFS(int num) {
-        if (num > n) {
-            System.out.println(arr);
-        } else {
-            //쓴다.
-            arr.add(num);
-            DFS(num + 1);
-            //안쓴다.
-            arr.remove(arr.size() - 1);
-            DFS(num + 1);
-
+        for (int i = idx; i <= n; i++) {
+            //i를 부분집합에 포함한다.
+            subSet.add(i);
+            dfs(i + 1);
+            //i를 부분집합에 포함하지 않는다.
+            subSet.remove((Object)i);
         }
+        System.out.println(subSet);
     }
-
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
-        DFS(1);
+        //1 - n 부분집합을 원소수가 많은 순으로 출력
+        set = new int[n];
+        subSet = new ArrayList<>();
+
+        for (int i = 1; i <= n; i++) {
+            set[i - 1] = i;
+        }
+
+        dfs(1);
 
     }
 }
