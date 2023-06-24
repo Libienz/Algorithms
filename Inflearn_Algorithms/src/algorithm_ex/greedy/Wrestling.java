e5404d5c59631925de7ea5311ee61176b206c9d4
Kuen Hee Lee <Leekh10195@gmail.com>
2023-06-25 02:53:16 +0900

    09-01 done

--- Inflearn_Algorithms/src/algorithm_ex/greedy/Wrestling.java
+++ Inflearn_Algorithms/src/algorithm_ex/greedy/Wrestling.java
@@ -2,66 +2,39 @@ package algorithm_ex.greedy;
 
 import java.util.*;
 
-class Spec {
-    private int w;
-    private int h;
 
-    public int getW() {
-        return w;
-    }
-
-    public void setW(int w) {
-        this.w = w;
-    }
-
-    public int getH() {
-        return h;
-    }
-
-    public void setH(int h) {
-        this.h = h;
-    }
-
-    public Spec(int w, int h) {
-        this.w = w;
-        this.h = h;
-    }
-}
 public class Wrestling {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int size = sc.nextInt();
-        ArrayList<Spec> arr = new ArrayList<>();
-        int res = 0;
-        boolean p = true;
 
-        for (int i = 0; i < size; i++) {
-            int h = sc.nextInt();
-            int w = sc.nextInt();
-            arr.add(new Spec(w, h));
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(); //n명의 지원자가 씨름선수 선발 공고에 지원했다.
+        int[] ha = new int[n]; //ha[i] -> i번선수의 키
+        int[] wa = new int[n]; //wa[i] -> i번선수의 몸무게
+        int res = n;
+
+        for (int i = 0; i < n; i++) {
+            int h = sc.nextInt(); //지원자의 키
+            int w = sc.nextInt(); //지원자의 몸무게
+            ha[i] = h;
+            wa[i] = w;
         }
 
-        for (int i = 0; i < size; i++) {
-            Spec cur = arr.get(i);
-            //cur가 선발될 수 있는지 알아보자
-            for (int j = 0; j < size; j++) {
+        for (int i = 0; i < n; i++) {
+            int ch = ha[i]; //보고 있는 선수의 키
+            int cw = wa[i]; //보고 있는 선수의 몸무게
+
+            for (int j = 0; j < n; j++) {
                 if (i == j) {
                     continue;
                 }
-                Spec comp = arr.get(j);
-                if (cur.getH() < comp.getH() && cur.getW() < comp.getW()) {
-                    //탈락
-                    p = false;
+                if (ch < ha[j] && cw < wa[j]) { //보고 있는 선수 보다 키도 몸무게도 높은 선수가 존재하면
+                    res--;
                     break;
                 }
             }
-            if (p) {
-                res++;
-            }
-            p = true;
-
-
         }
+
         System.out.println(res);
     }
 }
