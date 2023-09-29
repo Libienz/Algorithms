0a08fcfb51713890da018781a680faa06756bab7
Kuen Hee Lee <Leekh10195@gmail.com>
2023-09-29 22:21:28 +0900

    2141 done

--- boj/src/Q2141.java
+++ boj/src/Q2141.java
@@ -1,12 +1,10 @@
 import java.util.*;
 import java.io.*;
 
-//결정 알고리즘 시간초과
 public class Q2141 {
 
     private static int N;
-    private static int[] vPops; //마을 인구수
-    private static int[] vLocs; //마을 위치
+    private static ArrayList<Village> villages = new ArrayList<>();
     private static int leftVillageLoc = Integer.MAX_VALUE;
     private static int rightVillageLoc = Integer.MIN_VALUE;
     public static void main(String[] args) throws IOException {
@@ -14,45 +12,50 @@ public class Q2141 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         N = Integer.parseInt(br.readLine());
-
-        vLocs = new int[N];
-        vPops = new int[N];
-
+        long totalPop = 0L;
         for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
 
             int x = Integer.parseInt(st.nextToken()); //마을의 위치
             int a = Integer.parseInt(st.nextToken()); //마을의 인구수
-
-            //제일 왼쪽 마을과 오른쪽 마을 저장해놓기
-            if (x < leftVillageLoc) {
-                leftVillageLoc = x;
-            }
-            if (x > rightVillageLoc) {
-                rightVillageLoc = x;
-            }
-            vPops[i] = a;
-            vLocs[i] = x;
+            totalPop += a;
+            Village v = new Village(x, a);
+            villages.add(v);
         }
 
+        //위치 순 정렬
+        Collections.sort(villages);
         int res = Integer.MAX_VALUE;
-        int postLoc = 0;
-        //제일 왼쪽에 위치 한 마을과 제일 오른쪽에 있는 마을 사이에 답이 있다.
-        for (int i = leftVillageLoc; i <= rightVillageLoc; i++) {
 
-            //i 위치에 우체국을 세울 경우 각 사람들까지의 거리의 합
-            int td = 0;
-            for (int j = 0; j < vLocs.length; j++) {
-                int dist = Math.abs(vLocs[j] - i);
-                td += dist * vPops[j];
-            }
-            if (td < res) {
-                res = td;
-                postLoc = i;
+        long leftPop = 0;
+        long postLoc = 0;
+        for (int i = 0; i < villages.size(); i++) {
+            leftPop += villages.get(i).population;
+
+            if ((totalPop + 1) / 2 <= leftPop) {
+
+                postLoc = villages.get(i).location;
+                break;
             }
         }
+
         bw.write(postLoc +"");
         bw.flush();
 
+    }
+
+    static class Village implements Comparable<Village> {
+        private long location;
+        private long population;
+
+        public Village(long location, long population) {
+            this.location = location;
+            this.population = population;
+        }
+
+        @Override
+        public int compareTo(Village o) {
+            return (int) (this.location - o.location);
+        }
     }
 }
