01020d89a3cfd968a6cab3a4ff0e39708a68b9b7
이근희 / Lee Kuen Hee <Leekh10195@gmail.com>
2023-11-22 18:36:00 +0900

    [Bronze V] Title: A×B, Time: 148 ms, Memory: 15872 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int operand1 = Integer.parseInt(st.nextToken());
+        int operand2 = Integer.parseInt(st.nextToken());
+        bw.write(operand1 * operand2 + "\n");
+        bw.flush();
+    }
+}
