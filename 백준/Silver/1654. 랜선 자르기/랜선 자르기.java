4120901c5fcd2d1cf4292b5e84fc42c5a9241b20
이근희 / Lee Kuen Hee <Leekh10195@gmail.com>
2023-11-25 00:06:12 +0900

    [Silver II] Title: 랜선 자르기, Time: 268 ms, Memory: 25372 KB -BaekjoonHub

---
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -0,0 +1,59 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    private static int K;
+    private static int N;
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        K = Integer.parseInt(st.nextToken()); //가지고 있는 랜선 개수
+        N = Integer.parseInt(st.nextToken()); //필요한 랜선의 개수
+        List<Integer> cables = new ArrayList<>(); //초기 랜선 정보
+
+
+        for (int i = 0; i < K; i++) {
+            cables.add(Integer.parseInt(br.readLine()));
+        }
+
+        Long cutLengthMin = 0L;
+        Long cutLengthMax = (long) Integer.MAX_VALUE;
+        Long cutLength = 0L;
+        while ((cutLengthMax - cutLengthMin) > 1) {
+
+
+            cutLength = (cutLengthMax + cutLengthMin) / 2;
+            if (affordableLength(cutLength, cables)) {
+                cutLengthMin = cutLength;
+            } else {
+                cutLengthMax = cutLength;
+            }
+        }
+
+        if (cutLengthMin != cutLengthMax) {
+            if (affordableLength(cutLengthMax, cables)) {
+                bw.write(cutLengthMax + "");
+            } else {
+                bw.write(cutLengthMin + "");
+            }
+        }
+        bw.flush();
+
+    }
+
+
+    public static boolean affordableLength(Long cutLength, List<Integer> cables) {
+        Long count = 0L;
+        for (int cable : cables) {
+            count += cable / cutLength;
+        }
+        if (count >= N) {
+            return true;
+        }
+        return false;
+    }
+}
