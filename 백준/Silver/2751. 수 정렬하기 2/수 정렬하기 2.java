f521016a27728200e1b36f729d7e0d9a0244ebad
이근희 / Lee Kuen Hee <Leekh10195@gmail.com>
2023-11-30 14:35:34 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1556 ms, Memory: 147440 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        int[] numbers = new int[N];
+        for (int i = 0; i < N; i++) {
+            numbers[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(numbers);
+
+        for (int num : numbers) {
+            bw.write(String.valueOf(num) + "\n");
+        }
+        bw.flush();
+    }
+}
+
