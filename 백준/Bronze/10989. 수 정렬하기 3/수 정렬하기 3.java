01ca157d4eeb5ff709b25a66d405ce5304d9b388
이근희 / Lee Kuen Hee <Leekh10195@gmail.com>
2023-12-04 10:54:42 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 2704 ms, Memory: 345404 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    private static Deque<Integer> deque = new ArrayDeque<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] numbers = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            numbers[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(numbers);
+
+        for (int number : numbers) {
+            bw.write(String.valueOf(number) + "\n");
+        }
+        bw.flush();
+    }
+}
