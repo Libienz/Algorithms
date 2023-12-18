cc667c9e35da987731ca47abc6b4c94eece04db4
이근희 / Lee Kuen Hee <Leekh10195@gmail.com>
2023-12-18 23:25:52 +0900

    [Silver III] Title: 피보나치 함수, Time: 160 ms, Memory: 16060 KB -BaekjoonHub

---
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    private static Integer[][] dp = new Integer[41][2];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        dp[0][0] = 1;
+        dp[0][1] = 0;
+        dp[1][0] = 0;
+        dp[1][1] = 1;
+
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 0; i < T; i++) {
+            int N = Integer.parseInt(br.readLine());
+            fibonacci(N);
+            bw.write(dp[N][0] + " " + dp[N][1]);
+            bw.newLine();
+        }
+        bw.flush();
+    }
+    static Integer[] fibonacci(int N) {
+        if(dp[N][0] == null || dp[N][1] == null) {
+            dp[N][0] = fibonacci(N - 1)[0] + fibonacci(N - 2)[0];
+            dp[N][1] = fibonacci(N - 1)[1] + fibonacci(N - 2)[1];
+        }
+        return dp[N];
+    }
+
+}
