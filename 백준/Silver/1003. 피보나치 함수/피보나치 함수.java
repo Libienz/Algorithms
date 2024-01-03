a970ce3b83e3284db0f10a85435f4f7a26f1b586
이근희 / Lee Kuen Hee <Leekh10195@gmail.com>
2024-01-03 09:55:42 +0900

    [Silver III] Title: 피보나치 함수, Time: 136 ms, Memory: 14548 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -1,34 +1,30 @@
 import java.io.*;
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
-    private static Integer[][] dp = new Integer[41][2];
+
+    private static int[] zeroCounts = new int[41];
+    private static int[] oneCounts = new int[41];
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        dp[0][0] = 1;
-        dp[0][1] = 0;
-        dp[1][0] = 0;
-        dp[1][1] = 1;
-
         int T = Integer.parseInt(br.readLine());
+        zeroCounts[0] = 1;
+        zeroCounts[1] = 0;
+        oneCounts[0] = 0;
+        oneCounts[1] = 1;
         for (int i = 0; i < T; i++) {
-            int N = Integer.parseInt(br.readLine());
-            fibonacci(N);
-            bw.write(dp[N][0] + " " + dp[N][1]);
-            bw.newLine();
+            int num = Integer.parseInt(br.readLine());
+            for (int j = 2; j <= num; j++) {
+                zeroCounts[j] = zeroCounts[j - 2] + zeroCounts[j - 1];
+                oneCounts[j] = oneCounts[j - 2] + oneCounts[j - 1];
+            }
+
+            bw.write(String.format("%d %d\n", zeroCounts[num], oneCounts[num]));
         }
+
         bw.flush();
     }
-    static Integer[] fibonacci(int N) {
-        if(dp[N][0] == null || dp[N][1] == null) {
-            dp[N][0] = fibonacci(N - 1)[0] + fibonacci(N - 2)[0];
-            dp[N][1] = fibonacci(N - 1)[1] + fibonacci(N - 2)[1];
-        }
-        return dp[N];
-    }
-
 }
