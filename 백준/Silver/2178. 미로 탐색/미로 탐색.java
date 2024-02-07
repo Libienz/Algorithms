92e504e544f2428f093ce03ee61301bcb9565f61
리비 <Leekh10195@gmail.com>
2024-02-07 12:32:49 +0900

    [Silver I] Title: 미로 탐색, Time: 132 ms, Memory: 14596 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,18 +1,18 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-
-    private static int N;
-    private static int M;
     private static int[][] map;
     private static boolean[][] visited;
+    private static final int startRow = 0;
+    private static final int startCol = 0;
     private static int[] moveR = {-1, 1, 0, 0};
-    private static int[] moveC = {0, 0, 1, -1};
+    private static int[] moveC = {0, 0, -1, 1};
+    private static int N;
+    private static int M;
 
 
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -22,57 +22,53 @@ public class Main {
 
         map = new int[N][M];
         visited = new boolean[N][M];
+
         for (int i = 0; i < N; i++) {
             String row = br.readLine();
             for (int j = 0; j < M; j++) {
-                int cur = Integer.parseInt(row.charAt(j)+"");
-                map[i][j] = cur;
+                map[i][j] = row.charAt(j) - 48;
             }
         }
 
-        Node sn = new Node(0, 0);
-        visited[0][0] = true;
-        int res = countHop(sn);
-        bw.write(res + 1 + "");
-        bw.newLine();
+        int step = countStep();
+        bw.write(String.valueOf(step));
         bw.flush();
-
     }
 
-    public static int countHop(Node sn) {
+    public static int countStep() {
+        Queue<Point> q = new LinkedList<>();
+        int step = 0;
 
-        Queue<Node> q = new LinkedList<>();
-        int level = 0;
-        q.add(sn);
+        q.add(new Point(startRow, startCol));
+        visited[startRow][startCol] = true;
 
         while (!q.isEmpty()) {
             int len = q.size();
             for (int i = 0; i < len; i++) {
-                Node cur = q.poll();
-
+                Point cur = q.poll();
                 if (cur.r == N - 1 && cur.c == M - 1) {
-                    return level;
+                    return step + 1;
                 }
                 for (int j = 0; j < 4; j++) {
                     int nr = cur.r + moveR[j];
                     int nc = cur.c + moveC[j];
-                    //index out or visited or blocked
-                    if (nr < 0 || nc < 0 || nr >= N || nc >= M || map[nr][nc] == 0 || visited[nr][nc]) {
-                        continue;
+                    if (nr >= 0 && nr < N && nc >= 0 && nc < M && !visited[nr][nc] && map[nr][nc] == 1) {
+                        Point nextPoint = new Point(nr, nc);
+                        q.add(nextPoint);
+                        visited[nr][nc] = true;
                     }
-                    q.offer(new Node(nr, nc));
-                    visited[nr][nc] = true;
                 }
             }
-            level++;
+            step++;
         }
         return -1;
     }
-    static class Node {
+
+    static class Point {
         private int r;
         private int c;
 
-        public Node(int r, int c) {
+        public Point(int r, int c) {
             this.r = r;
             this.c = c;
         }
