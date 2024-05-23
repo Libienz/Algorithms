e986448403faa5a136b6e63267ed786a180550c7
리비 <Leekh10195@gmail.com>
2024-05-23 12:57:00 +0900

    [Silver III] Title: N과 M (2), Time: 132 ms, Memory: 14280 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    private static int N;
+    private static int M;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        dfs(1, new ArrayList<>(), bw);
+        bw.flush();
+    }
+
+    public static void dfs(int idx, List<Integer> choices, BufferedWriter bw) throws IOException {
+        if (choices.size() == M) {
+            for (int num : choices) {
+                bw.write(String.valueOf(num) + " ");
+            }
+            bw.newLine();
+            return;
+        }
+
+        for (int i = idx; i <= N; i++) {
+            choices.add(i);
+            dfs(i + 1, choices, bw);
+            choices.remove(choices.size() - 1);
+        }
+    }
+}
