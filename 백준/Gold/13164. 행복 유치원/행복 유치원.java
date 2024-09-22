234fb3b3ec2d15d1fdcc2b7a68a206464ec3b58d
리비 <Leekh10195@gmail.com>
2024-09-23 02:14:45 +0900

    [Gold V] Title: 행복 유치원, Time: 612 ms, Memory: 54680 KB -BaekjoonHub

--- 백준/Gold/13164. 행복 유치원/행복 유치원.java
+++ 백준/Gold/13164. 행복 유치원/행복 유치원.java
@@ -1,49 +1,34 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-
-    private static int N;
-    private static int K;
-    private static int[] heights;
-    private static int[] diffs;
-
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        N = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
-
-        heights = new int[N];
-        diffs = new int[N - 1];
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        
+        int[] heights = new int[n];
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            int h = Integer.parseInt(st.nextToken());
-            heights[i] = h;
+        for (int i = 0; i < n; i++) {
+            heights[i] = Integer.parseInt(st.nextToken());
         }
-
-        //키차이가 가장 많이나는 곳은 분리되면 좋음
-        for (int i = 0; i < N - 1; i++) {
-            int cur = heights[i];
-            int next = heights[i + 1];
-            int diff = next - cur;
-            diffs[i] = diff;
+        
+        int[] diffs = new int[n - 1];
+        for (int i = 0; i < n - 1; i++) {
+            diffs[i] = heights[i + 1] - heights[i];
         }
-
+        
         Arrays.sort(diffs);
-        int res = 0;
-        for (int i = 0; i < diffs.length - (K-1); i++) {
-//            System.out.println("diffs[i] = " + diffs[i]);
-            res += diffs[i];
+        
+        int result = 0;
+        for (int i = 0; i < n - k; i++) {
+            result += diffs[i];
         }
-        bw.write(res + "");
+        
+        bw.write(result + "\n");
         bw.flush();
-
     }
-
-
-
 }
