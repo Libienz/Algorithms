17129791017b6ec0df438b3266e8fcbfddbe7bbd
리비 <Leekh10195@gmail.com>
2024-09-23 01:00:43 +0900

    [Gold V] Title: 강의실 배정, Time: 596 ms, Memory: 69940 KB -BaekjoonHub

--- 백준/Gold/11000. 강의실 배정/강의실 배정.java
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.java
@@ -1,70 +1,33 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-
-    private static ArrayList<Lecture> lectures;
-    private static PriorityQueue<Integer> pq;
-
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        int N = Integer.parseInt(br.readLine());
-        pq = new PriorityQueue<>();
-        lectures = new ArrayList<>();
-
-        for (int i = 0; i < N; i++) {
+        
+        int n = Integer.parseInt(br.readLine());
+        int[][] lectures = new int[n][2];
+        
+        for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int s = Integer.parseInt(st.nextToken());
-            int f = Integer.parseInt(st.nextToken());
-            lectures.add(new Lecture(s, f));
+            lectures[i][0] = Integer.parseInt(st.nextToken());
+            lectures[i][1] = Integer.parseInt(st.nextToken());
         }
 
-        Collections.sort(lectures);
-        int prev = 0;
-        pq.add(lectures.get(0).finishTime);
-        for (int i = 1; i < lectures.size(); i++) {
-            Lecture cur = lectures.get(i);
-            Integer peek = pq.peek();
-//            System.out.println("peek = " + peek);
-            if (cur.startTime >= peek) {
-                //can concat
+        Arrays.sort(lectures, Comparator.comparingInt(a -> a[0]));
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        pq.add(lectures[0][1]);
+
+        for (int i = 1; i < n; i++) {
+            if (pq.peek() <= lectures[i][0]) {
                 pq.poll();
-                pq.add(cur.finishTime);
-            } else {
-                //cannot concat
-                pq.add(cur.finishTime);
             }
-
+            pq.add(lectures[i][1]);
         }
 
-        bw.write(pq.size() + "");
+        bw.write(pq.size() + "\n");
         bw.flush();
-    }
-
-    static class Lecture implements Comparable<Lecture> {
-
-        private int startTime;
-        private int finishTime;
-
-        public Lecture(int startTime, int finishTime) {
-            this.startTime = startTime;
-            this.finishTime = finishTime;
-        }
-
-        @Override
-        public int compareTo(Lecture o) {
-            return startTime - o.startTime;
-        }
-
-        @Override
-        public String toString() {
-            return "Lecture{" +
-                    "startTime=" + startTime +
-                    ", finishTime=" + finishTime +
-                    '}';
-        }
     }
 }
