dd4d78ea008bf3a779946cdf1ac1f6998a25a67e
리비 <Leekh10195@gmail.com>
2024-10-14 01:25:21 +0900

    [Gold IV] Title: 알파벳, Time: 796 ms, Memory: 14664 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -1,83 +1,49 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
+    private static int R;
+    private static int C;
+    private static char[][] grid;
+    private static int max = 1;
+    private static int[] dx = {-1, 1, 0, 0};
+    private static int[] dy = {0, 0, -1, 1};
 
-    static int r;
-    static int c;
-    static char[][] board;
-    static Set<Character> set;
-    static int maxMv;
-    public static boolean isAvailableIdx(int row, int col) {
-        if (0 <= row && row < r && 0 <= col && col < c) {
-            return true;
-        }
-        return false;
-    }
-    public static void dfs(int r, int c, int lev) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        R = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
 
-        int row = r;
-        int col = c;
-        if (maxMv < lev) {
-            maxMv = lev;
-        }
-
-        //상
-        row = row -1;
-        if (isAvailableIdx(row, col) && !set.contains(board[row][col])) {
-            set.add(board[row][col]);
-            dfs(row, col, lev + 1);
-            set.remove(board[row][col]);
-        }
-        //하
-        row = row + 2;
-        if (isAvailableIdx(row, col) && !set.contains(board[row][col])) {
-            set.add(board[row][col]);
-            dfs(row, col, lev + 1);
-            set.remove(board[row][col]);
-        }
-        //좌
-        row = row - 1;
-        col = col - 1;
-        if (isAvailableIdx(row, col) && !set.contains(board[row][col])) {
-            set.add(board[row][col]);
-            dfs(row, col, lev + 1);
-            set.remove(board[row][col]);
-        }
-        //우
-        col = col + 2;
-        if (isAvailableIdx(row, col) && !set.contains(board[row][col])) {
-            set.add(board[row][col]);
-            dfs(row, col, lev + 1);
-            set.remove(board[row][col]);
-        }
-    }
-    public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-        r = sc.nextInt();
-        c = sc.nextInt();
-
-        //r by c의 board가 있다.
-        board = new char[r][c];
-        for (int i = 0; i < r; i++) {
-            String row = sc.next();
-            for (int j = 0; j < c; j++) {
-                board[i][j] = row.charAt(j);
+        grid = new char[R][C];
+        for (int i = 0; i < R; i++) {
+            String line = br.readLine();
+            for (int j = 0; j < C; j++) {
+                grid[i][j] = line.charAt(j);
             }
         }
 
-        //0,0에 있는 알파벳은 말임
-        //말은 상하좌우로 움직일 수 있지만 한번 지나온 곳의 알파벳과 같은 알파벳을 가진 곳으로는 향할 수 없다.
-
-        set = new HashSet<>();
-        maxMv = Integer.MIN_VALUE;
-        set.add(board[0][0]);
-        dfs(0, 0, 1);
-
-        System.out.println(maxMv);
-
-
+        int initialMask = 1 << (grid[0][0] - 'A');
+        dfs(0, 0, initialMask, 1);
+        System.out.println(max);
     }
 
+    private static void dfs(int x, int y, int mask, int count) {
+        for (int dir = 0; dir < 4; dir++) {
+            int nx = x + dx[dir];
+            int ny = y + dy[dir];
 
+            if (nx >= 0 && nx < R && ny >= 0 && ny < C) {
+                int charBit = 1 << (grid[nx][ny] - 'A');
+                if ((mask & charBit) == 0) {
+                    if (count + 1 > max) {
+                        max = count + 1;
+                    }
+                    dfs(nx, ny, mask | charBit, count + 1);
+                }
+            }
+        }
+    }
 }
