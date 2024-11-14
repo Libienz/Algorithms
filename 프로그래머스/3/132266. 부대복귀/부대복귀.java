fb6882b023f812f9e021d8c55f270c2070e2d98e
리비 <Leekh10195@gmail.com>
2024-11-14 12:28:27 +0900

    [level 3] Title: 부대복귀, Time: 304.91 ms, Memory: 176 MB -BaekjoonHub

--- 프로그래머스/3/132266. 부대복귀/부대복귀.java
+++ 프로그래머스/3/132266. 부대복귀/부대복귀.java
@@ -1,9 +1,7 @@
 import java.util.*;
 
 class Solution {
-    
-    private static Map<Path, Integer> distanceCache = new HashMap<>();
-    
+
     public int[] solution(int n, int[][] roads, int[] sources, int destination) {
         List<Integer>[] map = new ArrayList[n];
         for (int i = 0; i<n; i++) {
@@ -16,30 +14,14 @@ class Solution {
             
             map[start-1].add(dest-1);
             map[dest-1].add(start-1);
-            
-            distanceCache.put(new Path(start-1, dest-1),1);
-            distanceCache.put(new Path(dest-1, start-1),1);
         }
         
-        int[] answer = new int[sources.length];
-        
-        for (int i = 0; i<sources.length; i++) {
-            int source = sources[i] - 1;
-            int dest = destination - 1;
-            int distance = calculateMinimumDistance(map, source, dest);
-            
-//             Path sourceToDest = new Path(source, dest);
-//             Path destToSource = new Path(dest, source);
-            
-//             distanceCache.put(sourceToDest, distance);
-//             distanceCache.put(destToSource, distance);
-            
-            answer[i] = distance;
-        }
-        return answer;
+        return calculateMinimumDistance(map, destination-1, sources);
     }
     
-    private int calculateMinimumDistance(List<Integer>[] map, int source, int destination) {
+    private int[] calculateMinimumDistance(List<Integer>[] map, int source, int[] destinations) {
+        int[] answer = new int[destinations.length];
+        Arrays.fill(answer, -1);
         Queue<Integer> q = new LinkedList<>();
         q.add(source);
         boolean[] visited = new boolean[map.length];
@@ -51,21 +33,10 @@ class Solution {
             
             for (int i = 0; i<len; i++) { 
                 int cur = q.poll();
-                Path pathToDestination = new Path(cur, destination);
-                if (distanceCache.containsKey(pathToDestination)) {
-                    int distanceToDest = distanceCache.get(pathToDestination);
-                    if (distanceToDest == -1) {
-                        return distanceToDest;
+                for (int j = 0; j<destinations.length; j++) {
+                    if (destinations[j]-1 == cur) {
+                        answer[j] = level;
                     }
-                    return distanceToDest + level;
-                }
-                // Path pathToHere = new Path(source, cur);
-                // Path pathToThere = new Path(cur, source);
-                // distanceCache.put(pathToHere, level);
-                // distanceCache.put(pathToThere, level);
-                
-                if (cur == destination) {
-                    return level;
                 }
                 for (int next : map[cur]) { 
                     if (visited[next]) {
@@ -78,33 +49,6 @@ class Solution {
             level++;
         }
         
-        return -1;
-    }
-    
-    private static class Path {
-        int start;
-        int dest;
-        
-        public Path(int start, int dest) {
-            this.start = start;
-            this.dest = dest;
-        }
-        
-        @Override
-        public boolean equals (Object target) {
-            if (this == target) {
-                return true;
-            }
-            if (target == null || getClass() != target.getClass()) {
-                return false;
-            }
-            Path path = (Path) target;
-            return start == path.start && dest == path.dest;
-        }
-        
-        @Override
-        public int hashCode() {
-            return Objects.hash(start, dest);
-        }
+        return answer;
     }
 }
