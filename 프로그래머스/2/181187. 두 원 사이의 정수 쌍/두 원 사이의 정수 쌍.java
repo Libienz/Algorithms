57b0ae99dd739d7d58d829661574d01c78226cba
리비 <Leekh10195@gmail.com>
2025-02-23 17:27:35 +0900

    [level 2] Title: 두 원 사이의 정수 쌍, Time: 21.89 ms, Memory: 90 MB -BaekjoonHub

--- 프로그래머스/2/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.java
+++ 프로그래머스/2/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.java
@@ -1,60 +1,49 @@
-import java.util.*;
-
 class Solution {
     public long solution(int r1, int r2) {
-        Circle c1 = Circle.createOriginCircle(r1);
-        Circle c2 = Circle.createOriginCircle(r2);
+        Circle smaller = Circle.createCenterCircle(r1);
+        Circle bigger = Circle.createCenterCircle(r2);
         
         long answer = 0;
-        for (int y = 1; y <= c2.radius; y++) {
-            int x1 = (int) Math.ceil(c1.getX(y));
-            int x2 = (int) Math.floor(c2.getX(y));
-            answer += x2 - x1 + 1;
+        for (int x = 1; x <= r2; x++) {
+            int by = (int) Math.floor(bigger.getY(x));
+            int sy = (int) Math.ceil(smaller.getY(x));
+            answer += (by - sy + 1);
         }
-        
         return answer * 4;
     }
     
+    
     private static class Circle {
-        int xPoint;
-        int yPoint;
-        int radius;
+        int x;
+        int y;
+        int r;
         
-        public Circle(int xPoint, int yPoint, int radius) {
-            this.xPoint = xPoint;
-            this.yPoint = yPoint;
-            this.radius = radius;
+        public Circle(int x, int y, int r) {
+            this.x = x;
+            this.y = y;
+            this.r = r;
         }
         
-        public double getX(int y) {
-            return Math.sqrt(Math.pow(radius,2) - Math.pow(y,2));
+        public static Circle createCenterCircle(int r) {
+            return new Circle(0, 0, r);
         }
         
-        public static Circle createOriginCircle(int radius) {
-            return new Circle(0,0,radius);
-        }
-        
-        public boolean contains(int x, int y) {
-            double distanceSquare = Math.pow(x - xPoint, 2) + Math.pow(y - yPoint, 2);
-            double radiusSquare = Math.pow(radius, 2);
+        public boolean contains(int tx, int ty) {
+            int xd = x - tx;
+            int yd = y - ty;
             
-            if (distanceSquare > radiusSquare) {
-                return false;
-            }
-            if (distanceSquare < radiusSquare) {
-                return true;
-            }
-            return false;
+            return Math.pow(r,2) > Math.pow(xd, 2) + Math.pow(yd, 2);
         }
         
-        public boolean isPointOnBoarderLine(int x, int y) {
-            double distanceSquare = Math.pow(xPoint - x, 2) + Math.pow(yPoint - y, 2);
-            double radiusSquare = Math.pow(radius, 2);
+        public boolean isPointOnBoarderLine(int tx, int ty) {
+            int xd = x - tx;
+            int yd = y - ty;
             
-            if (distanceSquare == radiusSquare) {
-                return true;
-            }
-            return false;
+            return Math.pow(r,2) == Math.pow(xd, 2) + Math.pow(yd, 2);
+        }
+        
+        public double getY(int tx) {
+            return Math.sqrt(Math.pow(r,2) - Math.pow(tx - x, 2));
         }
     }
 }
